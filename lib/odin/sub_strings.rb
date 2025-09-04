38dcef712d5f737366b54312e05f0a89bd19bc16
James Couball <jcouball@yahoo.com>
2025-09-04 16:41:02 -0700

    Newly generated project

---
+++ lib/odin/sub_strings.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "sub_strings/version"
+
+module Odin
+  module SubStrings
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/odin/sub_strings/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Odin
+  module SubStrings
+    VERSION = "0.1.0"
+  end
+end
