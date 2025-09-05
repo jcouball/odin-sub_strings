31f1d823b6755f60fa29bffb0cfae07804b409df
James Couball <jcouball@yahoo.com>
2025-09-05 09:10:34 -0700

    Implementation of Odin::SubStrings.substrings

--- lib/odin/sub_strings.rb
+++ lib/odin/sub_strings.rb
@@ -6,7 +6,46 @@ require_relative 'sub_strings/version'
 module Odin
   # Contains the SubStrings implementation
   module SubStrings
-    # Determine the number of times a set or words occurs in a given string
-    def self.substrings(string, dictionary); end
+    # Determine the number of times a set of words occurs in a given string
+    #
+    # Returns a hash whose keys are the words from the given dictionary (set of
+    # words) found in the string. The values are the number of occurances of that
+    # word. If a word from the dictionary is not found, it is not included in the
+    # result.
+    #
+    # Word search is not case sensative.
+    #
+    # @example
+    #   dictionary = ['this', 'is', 's', 'not found']
+    #   Odin::SubStrings.substrings('This is a string', dictionary)
+    #   #=> { 'this' => 1, 'is' => 2, 's' => 3 }
+    #
+    # @param string [String] the string to search
+    #
+    # @param dictionary [Array<String>] the set of words to count in the given string
+    #
+    # @return [Hash<String, Intege>}] the words from dictionary found in the given string and their count
+    #
+    def self.substrings(string, dictionary)
+      return {} if string.empty?
+
+      result = Hash.new(0)
+      dictionary.each do |word|
+        word_count = word_count(string, word)
+        result[word] += word_count if word_count.positive?
+      end
+      result
+    end
+
+    def self.word_count(string, word)
+      string = string.downcase
+      word = word.downcase
+      index = -1
+      count = 0
+      until (index = string.index(word, index + 1)).nil?
+        count += 1
+      end
+      count
+    end
   end
 end
--- spec/odin/sub_strings_spec.rb
+++ spec/odin/sub_strings_spec.rb
@@ -2,6 +2,60 @@
 
 RSpec.describe Odin::SubStrings do
   it 'has a version number' do
-    expect(Odin::SubStrings::VERSION).not_to be nil
+    expect(described_class::VERSION).not_to be nil
+  end
+
+  describe '.substrings' do
+    subject { described_class.substrings(string, dictionary) }
+
+    let(:string) { 'below' }
+    let(:dictionary) { %w[below be low going it i sit] }
+
+    context 'when given an empty string' do
+      let(:string) { '' }
+      it { is_expected.to eq({}) }
+    end
+
+    context 'when given a string and a dictionary with that string in it' do
+      let(:string) { 'below' }
+      let(:dictionary) { ['below'] }
+      it { is_expected.to eq({ 'below' => 1 }) }
+    end
+
+    context 'when the only dictionary work appears twice in the string' do
+      let(:string) { 'Sit it down' }
+      let(:dictionary) { ['it'] }
+      it { is_expected.to eq({ 'it' => 2 }) }
+    end
+
+    context 'with a single word' do
+      let(:string) { 'below' }
+      let(:dictionary) do
+        %w[
+          below down go going horn how howdy
+          it i low own part partner sit
+        ]
+      end
+      let(:expected_result) { { 'below' => 1, 'low' => 1 } }
+      it { is_expected.to eq(expected_result) }
+    end
+
+    context 'with multiple words' do
+      let(:string) { 'Howdy partner, sit down! How\'s it going?' }
+      let(:dictionary) do
+        %w[
+          below down go going horn how howdy
+          it i low own part partner sit
+        ]
+      end
+      let(:expected_result) do
+        {
+          'down' => 1, 'go' => 1, 'going' => 1, 'how' => 2,
+          'howdy' => 1, 'it' => 2, 'i' => 3, 'own' => 1,
+          'part' => 1, 'partner' => 1, 'sit' => 1
+        }
+      end
+      it { is_expected.to eq(expected_result) }
+    end
   end
 end
